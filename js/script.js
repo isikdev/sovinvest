1f6d08b647182f96fc87092b4706cfe0ef562b70
iskandar <isikjandev@gmail.com>
2022-09-11 18:58:56 +0500

    mobile

--- js/script.js
+++ js/script.js
@@ -9,3 +9,7 @@ $('.info__content-slider').slick({
 $(document).ready(function() {
     $("#navbar").sticky({ topSpacing: 0 });
 });
+let menu = $('.menu');
+$('.btn').on("click", function() {
+    menu.toggleClass('menu-active');
+});
